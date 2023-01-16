70110461bc44de29e9395708b552de7c16379356
NightShift3201 <112355963+NightShift3201@users.noreply.github.com>
2023-01-16 12:15:21 -0800

    no path redirect to home

--- main/views.py
+++ main/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Activity
 from django.db.models import Q
+from django.shortcuts import redirect
 
 def index(request):
-    return HttpResponse("Activity Connect!")
+    return redirect('/home')
 
 def home(request):
     activty_list = Activity.objects.all()
