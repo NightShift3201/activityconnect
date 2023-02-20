a0e7f63a78ec993477c8b4da40f17584f5cacf5c
NightShift3201 <112355963+NightShift3201@users.noreply.github.com>
2023-02-19 22:32:46 -0800

    Update settings.py

--- activityconnect/settings.py
+++ activityconnect/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/main')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/main/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
